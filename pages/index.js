41be9d8d155afa670c51fcd100fd3180bd940f7c
dilerbhangu <bhangutech@gmail.com>
2021-01-01 23:21:19 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>Hello World</div>;
+export default function Home({ d }) {
+  console.log("d" + d);
+  return <div>Hello Worlddd {d.name}</div>;
+}
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/api/hello");
+  const d = await res.json();
+  console.log("in get" + d);
+  return {
+    props: {
+      d,
+    },
+  };
 }
