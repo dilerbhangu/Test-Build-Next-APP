aec323ca19d62233d5d2f117ecd065107d47db98
dilerbhangu <bhangutech@gmail.com>
2021-01-01 23:23:48 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-export default function Home({ d }) {
-  console.log("d" + d);
-  return <div>Hello Worlddd {d.name}</div>;
+export default function Home() {
+  // console.log("d" + d);
+  return <div>Hello Worlddd </div>;
 }
 
-export async function getStaticProps() {
-  const res = await fetch("http://localhost:3000/api/hello");
-  const d = await res.json();
-  console.log("in get" + d);
-  return {
-    props: {
-      d,
-    },
-  };
-}
+// export async function getStaticProps() {
+//   const res = await fetch("http://localhost:3000/api/hello");
+//   const d = await res.json();
+//   console.log("in get" + d);
+//   return {
+//     props: {
+//       d,
+//     },
+//   };
+// }
