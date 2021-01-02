fe5a01b969ca2ac376fa87e91a657e388b5bb2b3
dilerbhangu <bhangutech@gmail.com>
2021-01-02 12:22:14 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-export default function Home() {
-  // return <div>Hello Worlddd {data[0].name}</div>;
-  return <div>Hello Worlddd </div>;
+export default function Home({ data }) {
+  return <div>Hello Worlddd {data[0].name}</div>;
+  // return <div>Hello Worlddd </div>;
 }
 
-// export async function getStaticProps() {
-//   const res = await fetch(process.env.API_PATH + "/api/hello");
-//   const data = await res.json();
-//   return {
-//     props: {
-//       data,
-//     },
-//   };
-// }
+export async function getStaticProps() {
+  const res = await fetch(process.env.API_PATH + "/api/hello");
+  const data = await res.json();
+  return {
+    props: {
+      data,
+    },
+  };
+}
